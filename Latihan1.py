4ad1011509001d83a4bed8aa03991c2e04bd8f25
FaizahviaFadhillah <faizahviavadhillah>
2022-12-21 21:15:28 +0700

    Python String

---
+++ Latihan1.py
@@ -0,0 +1,27 @@
+print("NAMA: Faizah Via Fadhillah")
+print("NIM  : 312210460")
+
+txt = 'Hello World'
+print()
+
+# jumlah karakternya
+print(len(txt))
+
+# karakter terakhir
+print(txt[-1])
+
+# karakter index ke-2 sampai index ke-4 (llo)
+print(txt[2:5])
+
+# spasi pada text tersebut (HelloWorld)
+print(txt.replace(" ", ""))
+
+# text menjadi huruf besar
+print(txt.upper())
+
+# text menjadi huruf kecil
+print(txt.lower())
+
+# Ganti karakter H dengan karakter J
+print(txt.replace("H", "J"))
+print()
---
+++ Latihan2.py
@@ -0,0 +1,6 @@
+print("NAMA : Faizah Via Fadhillah")
+print("NIM  : 312210460")
+
+umur = 24
+txt = "\nHello, nama saya john, dan umur saya adalah {0} tahun\n"
+print(txt.format(umur))
